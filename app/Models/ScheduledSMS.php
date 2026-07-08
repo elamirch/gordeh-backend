6d9314200f8056c563479a93951096ead6f20ce1
Amirreza <amirrezatm@protonmail.com>
2026-07-08 23:31:26 +0330

    Typo fixed

--- app/Models/ScheduledSMS.php
+++ app/Models/ScheduledSMS.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ScheduledSms extends Model
+class ScheduledSMS extends Model
 {
     protected $fillable = [
         'user_id',
